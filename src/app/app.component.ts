1616be18eb3abf451b02f8df6fff612eaa3d19b7
Rupesh <rupeshvm.977@gmail.com>
2018-04-17 02:04:21 +0530

    Add isLoggedIn in appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Geo-Tracker';
   isMobile: boolean;
-
+  isLoggedIn: boolean;
 
   constructor(
     // private media: ObservableMedia,
